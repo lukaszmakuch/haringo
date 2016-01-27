8511eb688cd5e5622a952c2c9befb34120386e07
Łukasz Makuch <kontakt@lukaszmakuch.pl>
2016-01-27 18:43:04 +0100

    ValueSourceResolverProxy

---
+++ src/BuildPlan/MethodCall/ParametersCollection/ValueSource/Resolver/Impl/ValueSourceResolverProxy.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of the ObjectBuilder library.
+ *
+ * @author Łukasz Makuch <kontakt@lukaszmakuch.pl>
+ * @license MIT http://opensource.org/licenses/MIT
+ */
+
+namespace lukaszmakuch\ObjectBuilder\BuildPlan\MethodCall\ParametersCollection\ValueSource\Resolver\Impl;
+
+use lukaszmakuch\ClassBasedRegistry\ClassBasedRegistry;
+use lukaszmakuch\ClassBasedRegistry\Exception\ValueNotFound;
+use lukaszmakuch\ObjectBuilder\BuildPlan\MethodCall\ParametersCollection\ValueSource\Resolver\Exception\ImpossibleToResolveValue;
+use lukaszmakuch\ObjectBuilder\BuildPlan\MethodCall\ParametersCollection\ValueSource\Resolver\ValueResolver;
+use lukaszmakuch\ObjectBuilder\BuildPlan\MethodCall\ParametersCollection\ValueSource\ValueSource;
+
+class ValueSourceResolverProxy implements ValueResolver
+{
+    private $actualResolvers;
+    
+    public function __construct()
+    {
+        $this->actualResolvers = new ClassBasedRegistry();
+    }
+    
+    public function registerResolver(ValueResolver $r, $targetValueSourceClass)
+    {
+        $this->actualResolvers->associateValueWithClasses($r, [$targetValueSourceClass]);
+    }
+    
+    public function resolveValueFrom(ValueSource $source)
+    {
+        try {
+            /* @var $suitableResolver ValueResolver */
+            $suitableResolver = $this->actualResolvers->fetchValueByObjects([$source]);
+            return $suitableResolver->resolveValueFrom($source);
+        } catch (ValueNotFound $e) {
+            throw new ImpossibleToResolveValue();
+        }
+    }
+    
+}
---
+++ tests/BuildingPlan/MethodCall/ParametersCollection/ValueSource/Resolver/Impl/ValueSourceResolverProxyTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use lukaszmakuch\ObjectBuilder\BuildPlan\MethodCall\ParametersCollection\ValueSource\Resolver\Exception\ImpossibleToResolveValue;
+use lukaszmakuch\ObjectBuilder\BuildPlan\MethodCall\ParametersCollection\ValueSource\Resolver\Impl\ValueSourceResolverProxy;
+use lukaszmakuch\ObjectBuilder\BuildPlan\MethodCall\ParametersCollection\ValueSource\Resolver\ValueResolver;
+use lukaszmakuch\ObjectBuilder\BuildPlan\MethodCall\ParametersCollection\ValueSource\ValueSource;
+
+/**
+ * This file is part of the ObjectBuilder library.
+ *
+ * @author Łukasz Makuch <kontakt@lukaszmakuch.pl>
+ * @license MIT http://opensource.org/licenses/MIT
+ */
+
+
+class TestValueSourceA implements ValueSource {}
+class TestValueSourceB implements ValueSource {}
+
+class ValueSourceResolverProxyTest extends PHPUnit_Framework_TestCase
+{
+    public function testCorrectDelegation()
+    {
+        $valueSourceA = new TestValueSourceA();
+        $valueSourceB = new TestValueSourceB();
+        
+        $resolverA = $this->getMock(ValueResolver::class);
+        $resolverA->method("resolveValueFrom")->will($this->returnValueMap([
+            [$valueSourceA, "a"]
+        ]));
+        
+        $resolverB = $this->getMock(ValueResolver::class);
+        $resolverB->method("resolveValueFrom")->will($this->returnValueMap([
+            [$valueSourceB, "b"]
+        ]));
+        
+        $proxy = new ValueSourceResolverProxy();
+        $proxy->registerResolver($resolverA, TestValueSourceA::class);
+        $proxy->registerResolver($resolverB, TestValueSourceB::class);
+        
+        $this->assertEquals("a", $proxy->resolveValueFrom($valueSourceA));
+        $this->assertEquals("b", $proxy->resolveValueFrom($valueSourceB));
+    }
+    
+    public function testUndefinedActualResolver()
+    {
+        $proxy = new ValueSourceResolverProxy();
+        $this->setExpectedException(ImpossibleToResolveValue::class);
+        $proxy->resolveValueFrom(new TestValueSourceA());
+    }
+}
