02f607bd5f6f54a9f2081b404dcff2999e354ba4
Łukasz Makuch <kontakt@lukaszmakuch.pl>
2016-01-31 09:15:54 +0100

    StaticFactoryProductBuilder refactor

--- src/Impl/StaticFactoryProductBuilder.php
+++ src/Impl/StaticFactoryProductBuilder.php
@@ -12,34 +12,47 @@ namespace lukaszmakuch\ObjectBuilder\Impl;
 use lukaszmakuch\ObjectBuilder\BuildPlan\BuildPlan;
 use lukaszmakuch\ObjectBuilder\BuildPlan\Impl\StaticFactoryProductBuildPlan;
 use lukaszmakuch\ObjectBuilder\Exception\ImpossibleToFinishBuildPlan;
+use lukaszmakuch\ObjectBuilder\MethodSelector\MethodSelector;
+use ReflectionClass;
 
 class StaticFactoryProductBuilder extends BuilderTpl
 {
     public function buildObjectBasedOn(BuildPlan $p)
     {
         /* @var $p StaticFactoryProductBuildPlan */
-        $reflectedFactoryClass = $this->getReflectedClassBasedOn($p->getFactoryClass());
-        
-        $allMatchingMethods = $this->findMatchingMethods(
-            $reflectedFactoryClass,
+        $factoryMethod = $this->findFactoryMethod(
+            $this->getReflectedClassBasedOn($p->getFactoryClass()),
             $p->getFactoryMethodCall()->getSelector()
         );
-        
-        if (count($allMatchingMethods) != 1) {
-            throw new ImpossibleToFinishBuildPlan(
-                "factory method selector matches different than 1 number of methods"
-            );
-        }
-        
-        $factoryMethod = $allMatchingMethods[0];
-        
+
         $builtObject = $this->callSpecifiedMethod(
             $factoryMethod,
             null, //static
             $p->getFactoryMethodCall()->getAssignedParamValues()
         );
-        
+
         $this->buildPlanByBuildObject->attach($builtObject, $p);
         return $builtObject;
+    }
+    
+    private function findFactoryMethod(
+        ReflectionClass $reflectedFactoryClass,    
+        MethodSelector $factoryMethodSelector
+    ) {
+        $allMatchingMethods = $this->findMatchingMethods(
+            $reflectedFactoryClass,
+            $factoryMethodSelector
+        );
+        $this->throwExceptionIfWrongNumberOfMethods($allMatchingMethods);
+        return $allMatchingMethods[0];
+    }
+    
+    private function throwExceptionIfWrongNumberOfMethods(array $reflectedFactoryMethods)
+    {
+        if (count($reflectedFactoryMethods) != 1) {
+            throw new ImpossibleToFinishBuildPlan(
+                "factory method selector matches different than 1 number of methods"
+            );
+        }
     }
 }
